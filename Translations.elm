5cb21d3295d06a9cf64a85300b124b483553b472
André Dietrich <andredietrich@web.de>
2021-03-02 17:47:16 +0100

    feat: Adds additional helper function & formatting
    1. `getLnFromCode` will Maybe return a Lang-type, which might be usefull
       for working with defaults
    2. `getCodefromLn` defines the inverted function to `getLnFromCode`
    3. Code is generated in compliance to elm-format

--- Translations.elm
+++ Translations.elm
@@ -1,30 +1,65 @@
 module Translations exposing (..)
 
+
 type Lang
-  =  De
-  |  En
+    = De
+    | En
 
-getLnFromCode: String -> Lang
+
+{-| Pass a language code that will return a Lang-type, if it exists.
+Otherwise `Nothing` is returned.
+-}
+getLnFromCode : String -> Maybe Lang
 getLnFromCode code =
-   case code of 
-      "de" -> De
-      "en" -> En
-      _ -> En
+    case String.toLower code of 
+        "de" ->
+            Just De
 
-hello: Lang -> String
-hello lang  =
-  case lang of 
-      De -> "Hallo"
-      En -> "Hello"
+        "en" ->
+            Just En
 
-gooddaySalute: Lang -> String -> String -> String
+        _ ->
+            Nothing
+
+
+{-| Return the lowerCase language code for the given Lang.
+-}
+getCodeFromLn : Lang -> String
+getCodeFromLn lang =
+    case lang of 
+        De ->
+            "de"
+
+        En ->
+            "en"
+
+
+hello : Lang -> String
+hello lang =
+    case lang of 
+        De ->
+            "Hallo"
+
+        En ->
+            "Hello"
+
+
+gooddaySalute : Lang -> String -> String -> String
 gooddaySalute lang str0 str1 =
-  case lang of 
-      De -> "Guten Tag " ++ str0 ++ " " ++ str1 ++ ""
-      En -> "Good Day " ++ str0 ++ " " ++ str1 ++ ""
+    case lang of 
+        De ->
+            "Guten Tag " ++ str0 ++ " " ++ str1 ++ ""
 
-tigersRoar: Lang -> String
-tigersRoar lang  =
-  case lang of 
-      De -> "Brüll!"
-      En -> "Roar!"
+        En ->
+            "Good Day " ++ str0 ++ " " ++ str1 ++ ""
+
+
+tigersRoar : Lang -> String
+tigersRoar lang =
+    case lang of 
+        De ->
+            "Brüll!"
+
+        En ->
+            "Roar!"
+
--- src/index.js
+++ src/index.js
@@ -1,121 +1,153 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra');
-const path = require('path');
+const fs = require('fs-extra')
+const path = require('path')
 
-const argv = require('yargs').argv;
-const R = require('ramda');
+const argv = require('yargs').argv
+const R = require('ramda')
 
 const [
   localeDir = path.join(__dirname, '../locale'),
   trFile = path.join(__dirname, '../Translations.elm'),
-] = argv._;
+] = argv._
 
 const files = fs.readdirSync(localeDir)
-const getLangFromFile = R.pipe(R.split('.'), R.slice(1, 2), R.head);
-const languages = R.map(getLangFromFile)(files);
-const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+const getLangFromFile = R.pipe(R.split('.'), R.slice(1, 2), R.head)
+const languages = R.map(getLangFromFile)(files)
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1)
 
 const tags = R.pipe(
-  R.addIndex(R.map)(
-    (tag, index) => {
-      const concatVal = index !== 0 ? '  |  ' : '  =  ';
-      return R.pipe(capitalize, R.concat(concatVal))(tag);
-    }),
-  R.join('\n')
-);
-const unionType =
-`type Lang\n${tags(languages)}`;
+  R.addIndex(R.map)((tag, index) => {
+    const concatVal = index !== 0 ? '    | ' : '    = '
+    return R.pipe(capitalize, R.concat(concatVal))(tag)
+  }),
+  R.join('\n'),
+)
+const unionType = `type Lang
+${tags(languages)}
+`
 
-const lnCase = ln => `      "${ln}" -> ${capitalize(ln)}`;
-const getLnFromCode =
-`getLnFromCode: String -> Lang
+const lnCase = (ln) => `        "${ln}" ->
+            Just ${capitalize(ln)}
+`
+const getLnFromCode = `{-| Pass a language code that will return a Lang-type, if it exists.
+Otherwise \`Nothing\` is returned.
+-}
+getLnFromCode : String -> Maybe Lang
 getLnFromCode code =
-   case code of \n${R.pipe(R.map(lnCase), R.join('\n'))(languages)}
-      _ -> En`;
+    case String.toLower code of \n${R.pipe(
+      R.map(lnCase),
+      R.join('\n'),
+    )(languages)}
+        _ ->
+            Nothing
+`
 
-const getFileContent =
-  filename => fs.readJsonSync(path.join(localeDir, filename), 'utf8');
+const codeCase = (ln) => `        ${capitalize(ln)} ->
+            "${ln}"
+`
+const getCodeFromLn = `{-| Return the lowerCase language code for the given Lang.
+-}
+getCodeFromLn : Lang -> String
+getCodeFromLn lang =
+    case lang of \n${R.pipe(R.map(codeCase), R.join('\n'))(languages)}
+`
 
-const writeFileContent = content => {
+const getFileContent = (filename) =>
+  fs.readJsonSync(path.join(localeDir, filename), 'utf8')
+
+const writeFileContent = (content) => {
   const generateTrElmModule = (snippets) =>
     `module Translations exposing (..)
+
 
 ${unionType}
 
 ${getLnFromCode}
 
-${R.join('\n\n', snippets)}`;
+${getCodeFromLn}
+${R.join('\n', snippets)}`
 
   fs.writeFileSync(trFile, generateTrElmModule(content), 'utf8')
-};
+}
 
-const trCase = ({ ln, value }) => `      ${capitalize(ln)} -> "${value}"`;
+const trCase = ({ ln, value }) =>
+  `        ${capitalize(ln)} ->
+            "${value}"
+`
 
 const generateElmFunctions = (translations, key) => {
-  const placeholderRegex = /\{\{.*?\}\}/;
-  const placeholderRegexGlobal = /\{\{.*?\}\}/g;
-  const [{ value: firstTr }] = translations;
-  const matches = firstTr.match(placeholderRegexGlobal) || [];
-  const signatureStrings = R.pipe(R.repeat('String'), R.join(' -> '));
+  const placeholderRegex = /\{\{.*?\}\}/
+  const placeholderRegexGlobal = /\{\{.*?\}\}/g
+  const [{ value: firstTr }] = translations
+  const matches = firstTr.match(placeholderRegexGlobal) || []
+  const signatureStrings = R.pipe(R.repeat('String'), R.join(' -> '))
   const args = R.pipe(
-    R.addIndex(R.map)((match, index) => ('str' + index)),
-    R.join(' ')
-  );
-  const replacePlaceholdersWrapper = (str) => replacePlaceholders(str, 0);
+    R.addIndex(R.map)((match, index) => 'str' + index + ' '),
+    R.join(''),
+  )
+
+  const replacePlaceholdersWrapper = (str) => replacePlaceholders(str, 0)
   const replacePlaceholders = (str, level) => {
     if (R.test(placeholderRegex, str)) {
-      const newStr = R.replace(placeholderRegex, `" ++ str${level} ++ "`, str);
-      return replacePlaceholders(newStr, level + 1);
+      const newStr = R.replace(placeholderRegex, `" ++ str${level} ++ "`, str)
+      return replacePlaceholders(newStr, level + 1)
     }
-    return str;
-  };
+    return str
+  }
+
   const strings = R.pipe(
-    R.map(R.pipe((translation) => (
-      R.merge(translation, {
-        value: replacePlaceholdersWrapper(translation.value)
-      })
-    ), trCase)),
-    R.join('\n')
-  );
-  return `${key}: Lang -> ${signatureStrings(matches.length + 1)}
-${key} lang ${args(matches)} =
-  case lang of \n${strings(translations)}`;
-};
+    R.map(
+      R.pipe(
+        (translation) =>
+          R.merge(translation, {
+            value: replacePlaceholdersWrapper(translation.value),
+          }),
+        trCase,
+      ),
+    ),
+    R.join('\n'),
+  )
+
+  return `${key} : Lang -> ${signatureStrings(matches.length + 1)}
+${key} lang ${args(matches)}=
+    case lang of \n${strings(translations)}
+`
+}
 
 const processFileContent = R.curry((ln, trObject) => {
-  const restructureData = (value, prefix, initialValue) => R.pipe(
-    R.keys,
-    R.reduce((acc, key) => {
-      const subValue = value[key];
-      const prefixedKey = prefix + (prefix ? capitalize(key) : key);
-      if (R.is(Object, subValue)) {
-        return restructureData(subValue, prefixedKey, acc);
-      }
-      return R.merge(acc, { [prefixedKey]: { ln, key: prefixedKey, value: subValue } });
-    }, initialValue)
-  )(value);
-  return restructureData(trObject, '', {});
+  const restructureData = (value, prefix, initialValue) =>
+    R.pipe(
+      R.keys,
+      R.reduce((acc, key) => {
+        const subValue = value[key]
+        const prefixedKey = prefix + (prefix ? capitalize(key) : key)
+        if (R.is(Object, subValue)) {
+          return restructureData(subValue, prefixedKey, acc)
+        }
+        return R.merge(acc, {
+          [prefixedKey]: { ln, key: prefixedKey, value: subValue },
+        })
+      }, initialValue),
+    )(value)
+  return restructureData(trObject, '', {})
   //return R.mapObjIndexed((value, key) => ({ ln, key, value }))(trObject);
-});
+})
 
-const createFileContentPairs = R.chain(
-  filename => R.pipe(
+const createFileContentPairs = R.chain((filename) =>
+  R.pipe(
     getFileContent,
     processFileContent(getLangFromFile(filename)),
-    R.values
-  )(filename)
-);
+    R.values,
+  )(filename),
+)
 
 const fileContentMap = R.pipe(
   createFileContentPairs,
-  R.groupBy(R.prop('key'))
-)(files);
+  R.groupBy(R.prop('key')),
+)(files)
 
-const createSnippets = R.pipe(
-  R.mapObjIndexed(generateElmFunctions),
-  R.values
-);
+const createSnippets = R.pipe(R.mapObjIndexed(generateElmFunctions), R.values)
 
-writeFileContent(createSnippets(fileContentMap));
+writeFileContent(createSnippets(fileContentMap))
 process.exit(0)
